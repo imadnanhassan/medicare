2caf10316684ed41a76a78069c15533e258f66fa
dev-abulhassan <abulhassan.dev@gmail.com>
2023-08-20 00:18:49 +0600

    about

--- fontrend/src/pages/SectionComponents/DoctorDetails/DoctorAbout.jsx
+++ fontrend/src/pages/SectionComponents/DoctorDetails/DoctorAbout.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const DoctorAbout = () => {
    return (
       <div>
-         Doctor About
+        <h3 className='text-[20px]'>About of
+         <span>Adnan Hassan</span>
+        </h3>
+
       </div>
    );
 };
