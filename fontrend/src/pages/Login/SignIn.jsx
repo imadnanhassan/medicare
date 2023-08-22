78eb2f2e2dffc4bd862d303d02b937c8a5dba97c
dev-abulhassan <abulhassan.dev@gmail.com>
2023-08-23 01:04:00 +0600

    login page

--- fontrend/src/pages/Login/SignIn.jsx
+++ fontrend/src/pages/Login/SignIn.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const SignIn = () => {
-   return (
-      <div>
-         Login 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <section className="px-5 lg:px-6">
+      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+          Hello <span className="text-primaryColor">Welcome</span> Back 💐
+        </h3>
+
+        <form className="py-4 md:py-0">
+          <div className="mb-5">
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+              className="w-full px-4 py-3 border-b border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+              className="w-full px-4 py-3 border-b border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+            />
+          </div>
+        </form>
       </div>
-   );
+    </section>
+  );
 };
 
 export default SignIn;
