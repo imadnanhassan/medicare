8349fd0518867e9104dc551c915061f141621e89
dev-abulhassan <abulhassan.dev@gmail.com>
2023-08-24 02:01:16 +0600

    signup page

--- fontrend/src/pages/Login/SignUp.jsx
+++ fontrend/src/pages/Login/SignUp.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React from "react";
+import signUpImg from "../../assets/images/signup.gif";
 
 const SignUp = () => {
-   return (
-      <div>
-         Regester
+  return (
+    <section className="px-5 lg:px-0">
+      <div className="max-w-[1170px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2 ">
+          <div className="hidden lg:block bg-primaryColor rounded-lg">
+            <figure className="rounded-l-lg">
+              <img src={signUpImg} alt="" className="w-full rounded-l-lg" />
+            </figure>
+          </div>
+
+          <div className="py-10 rounded-l-lg lg:pl-16">
+            <h3 className="text-headingColor text-[22px] font-bold mb-10 leading-9">
+              Create an <span className="text-primaryColor">Account</span>
+            </h3>
+            <form className="py-4 md:py-0 px-3 md:px-0">
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Enter Your Full Name"
+                  name="name"
+                  value=""
+                  required
+                  className="w-full pr-4 px-4 py-3 border-b border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="email"
+                  placeholder="Enter Your Email"
+                  name="email"
+                  value=""
+                  required
+                  className="w-full pr-4 px-4 py-3 border-b border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="password"
+                  placeholder="Enter Your Password"
+                  name="password"
+                  value=""
+                  required
+                  className="w-full pr-4 px-4 py-3 border-b border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                />
+              </div>
+
+              <div className="mb-5 flex items-center justify-between">
+                <label className="text-headingColor font-bold text-[16px] leading-7">
+                  Are you a:
+                  <select
+                    name="role"
+                    className="text-textColor font-semibold text-[15px] px-4 py-3 focus:outline-none"
+                  >
+                    <option value="patient">Patient</option>
+                    <option value="doctor">Doctor</option>
+                  </select>
+                </label>
+
+                <label className="text-headingColor font-bold text-[16px] leading-7">
+                  Gender:
+                  <select
+                    name="gender"
+                    className="text-textColor font-semibold text-[15px] px-4 py-3 focus:outline-none"
+                  >
+                    <option value="">Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">other</option>
+                  </select>
+                </label>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-   );
+    </section>
+  );
 };
 
 export default SignUp;
